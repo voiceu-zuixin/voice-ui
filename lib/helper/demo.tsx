15038bfca7780fed8847e7fa78203fbf3bc28725
voiceu <voiceu@qq.com>
2021-07-05 19:22:19 +0800

    代码高亮

--- lib/helper/demo.tsx
+++ lib/helper/demo.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
-
+import Highlight, { defaultProps } from "prism-react-renderer";
 interface Props {
     code: string
 }
 
 const Demo: React.FunctionComponent<Props> = (props) => {
+    
     return (
         <div>
             {props.children}
-            <pre>
-                {props.code}
-            </pre>
+            <Highlight {...defaultProps} code={props.code} language="jsx">
+                {({ className, style, tokens, getLineProps, getTokenProps }) => (
+                    <pre className={className} style={style}>
+                        {tokens.map((line, i) => (
+                            <div {...getLineProps({ line, key: i })}>
+                                {line.map((token, key) => (
+                                    <span {...getTokenProps({ token, key })} />
+                                ))}
+                            </div>
+                        ))}
+                    </pre>
+                )}
+            </Highlight>
         </div>
     )
 }
